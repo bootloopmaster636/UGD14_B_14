b7ddf3715d3fab5e2657bbacf43cd22d0b5fabcd
bootloopmaster636 <christhartono@ohtmail.com>
2023-06-16 13:15:56 +0700

    Initial commit

---
+++ src/ugd14_b_14/UGD14_B_14.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ugd14_b_14;
+
+/**
+ *
+ * @author tkg
+ */
+public class UGD14_B_14 {
+	public static void main(String[] args) {
+		// TODO code application loic here
+	}
+}
